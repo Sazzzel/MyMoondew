40bf1563f263f19ece8ac781ebc2759d5c39327c
Sazzzel <sazzzel@gmail.com>
2024-10-12 15:26:53 +0100

    add fullscreen

--- game.js
+++ game.js
@@ -3,12 +3,18 @@ let ctx = canvas.getContext("2d");
 let x = 0;
 let mode = 0; //0 = empty hands, 1 = hoe, 2 = trowel, 3 = watering can, 4 = sythe, 5 = fertalizer bucket, 6 = axe, 7 = pickaxe, 8 = sword, 9 = net.
 let tools = [];
+let fullScreenModal = new Image();
+fullScreenModal.src = "./assets/images/FullScreenModal.png";
 
 function gameLoop (){
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 1920, 1080);
     character.draw();
     actionBar.draw(ctx, mode);
+    if (!fullscreen){
+        ctx.drawImage(fullScreenModal, 0, 0);
+    }
+    
     requestAnimationFrame(gameLoop);
 }
 
